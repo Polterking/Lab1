442751c4be83a94461ec08ee4b78751156d9d3ee
Lab 1 <lab1@mail.ru>
2024-05-04 17:54:11 +0300

    Добавил числа

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "sdt.h"
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Введите первое целое число: ";
+    std::cin >> num1;
+
+    std::cout << "Введите второе целое число: ";
+    std::cin >> num2;
+
+    // Ваш код здесь
+
+    return 0;
+}
