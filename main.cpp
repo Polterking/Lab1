32fab72c6a71bde6c6949bbee65c2f6742b59bbf
USER2 <lab2@mail.ru>
2024-05-05 14:46:29 +0300

    В этой версии программы я добавил вычисление и печать разности введенных чисел. Также я заменил все русские строки на английские

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
-#include "sdt.h"
 #include <iostream>
 
 int main() {
     int num1, num2;
 
-    std::cout << "Введите первое целое число: ";
+    std::cout << "Enter the first integer: ";
     std::cin >> num1;
 
-    std::cout << "Введите второе целое число: ";
+    std::cout << "Enter the second integer: ";
     std::cin >> num2;
 
     int sum = num1 + num2;
-    std::cout << "Сумма введенных чисел: " << sum << std::endl;
+    std::cout << "The sum of the entered numbers: " << sum << std::endl;
+
+    int difference = num1 - num2;
+    std::cout << "The difference of the entered numbers: " << difference << std::endl;
 
     return 0;
 }
